89851d03a2f0b16fc33b6343d951ffc2d41f155d
Matt <i.w.b.s.bilm@gmail.com>
2022-01-16 15:54:15 -0800

    deployed on heroku

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,20 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from '@emotion/styled/'
 import TinderCard from 'react-tinder-card'
+import axios from './axios'
 
 function TinderCards() {
 
-    const [people, setPeople] = useState([
-        {name: 'Elon Musk',
-        url: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg'
+    const [people, setPeople] = useState([]);
 
-    },{name: 'Jeff Bezos',
-    url: 'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5bb22ae84bbe6f67d2e82e05%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D627%26cropX2%3D1639%26cropY1%3D129%26cropY2%3D1142'
-    
-        },
+    async function fetchData(){
+        const req = await axios.get('/tinder/cards');
+        setPeople(req.data);
+    }
 
-    ])
 
+    useEffect(() => {
+      
+    fetchData();
+    },[] )
+
+    console.table(people)
     const swiped = (direction, nameToDelete)=>{
     // setLastDirection(direction)
     }
@@ -33,7 +37,7 @@ function TinderCards() {
                 onSwipe={(dir)=>swiped(dir,person.name)}
                 onCardLeftScreen={()=>outOfFrame(person.name)}
                  >
-                <div style={{backgroundImage:`url(${person.url})`}}
+                <div style={{backgroundImage:`url(${person.imgUrl})`}}
                     className='card'>
                         <h3>{person.name}</h3>
                 </div>
---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: " https://tinder-back-end-mh.herokuapp.com"
+})
+
+
+export default instance;
